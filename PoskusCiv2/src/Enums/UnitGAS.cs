c6a699c2f2c4ca2a3f39da5d01e00d9fb99e2f79
mm <kazgaroth011@yahoo.com>
2018-12-24 19:42:25 +0100

    Some new files

---
+++ PoskusCiv2/src/Enums/UnitGAS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoskusCiv2.Enums
+{
+    public enum UnitGAS
+    {
+        Ground = 0,
+        Air = 1,
+        Sea = 2
+    }
+}
