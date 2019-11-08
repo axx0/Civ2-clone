297e1afa5505090983d71c6bdf61b512cc05889f
mm <kazgaroth011@yahoo.com>
2019-11-08 22:31:04 +0100

    more config progress

--- PoskusCiv2/src/Forms/IntroForm.cs
+++ PoskusCiv2/src/Forms/IntroForm.cs
@@ -18,6 +18,7 @@ namespace RTciv2.Forms
         private TextBox PathBox, SAVbox, ResultBox;
         public List<Resolution> Resolutions = new List<Resolution>();
         private string Civ2Path, SAVpath;
+        private Civ2button StartButton;
 
         public IntroForm()
         {
@@ -49,13 +50,13 @@ namespace RTciv2.Forms
             Controls.Add(ResolBox);
 
             //Start button
-            Civ2button StartButton = new Civ2button {
+            StartButton = new Civ2button {
                 Location = new Point(10, 400),
                 Size = new Size(100, 30),
                 Text = "Start Game" };
             Controls.Add(StartButton);
             StartButton.Click += new EventHandler(StartButton_Clicked);
-
+           
             //Quit button
             Civ2button QuitButton = new Civ2button {
                 Location = new Point(120, 400),
@@ -71,7 +72,7 @@ namespace RTciv2.Forms
                 Font = new Font("Times New Roman", 11),
                 Size = new Size(160, 30) };
             Controls.Add(PathBox);
-
+            
             //SAV name textbox
             SAVbox = new TextBox {
                 Location = new Point(30, 280),
@@ -103,20 +104,35 @@ namespace RTciv2.Forms
                 {
                     switch (count)
                     {
-                        case 0: Civ2Path = file.ReadLine(); break;  //1st # is civ2 path
-                        case 1: SAVpath = file.ReadLine(); break;   //2nd # is SAV name
-                        case 2: //3rd # is resolution
+                        case 0:     //1st # is civ2 path
+                            {
+                                Civ2Path = file.ReadLine();
+                                PathBox.Text = Civ2Path;
+                                break;                            
+                            }
+                        case 1:     //2nd # is SAV name
+                            {
+                                string SAVname = file.ReadLine();
+                                SAVpath = Civ2Path + SAVname + ".SAV";
+                                SAVbox.Text = SAVname;
+                                break;
+                            }
+                        case 2:     //3rd # is resolution
                             {
                                 line = file.ReadLine();
-                                if (line == "-1") FullscrBox.Checked = true;
-                                else ResolBox.SelectedIndex = Resolutions.FindIndex(a => a.Name == line) - 1;
+                                if (line == "-1") { FullscrBox.Checked = true; ResolBox.Enabled = false; }
+                                else { ResolBox.Enabled = true;  ResolBox.SelectedIndex = Resolutions.FindIndex(a => a.Name == line) - 1; FullscrBox.Checked = false; }
                                 break;
                             }
                         default: break;
                     }
                     count++;
                 }
-            }
+            }                        
+            CheckPaths();   //Check if there are any problems with input from config file
+
+            PathBox.TextChanged += new EventHandler(PathBox_TextChanged);   //Subscribe to text change events after config file was read, so that it doesn't interfere
+            SAVbox.TextChanged += new EventHandler(SAVbox_TextChanged);
         }
 
         private void IntroForm_Paint(object sender, PaintEventArgs e) {
@@ -134,31 +150,68 @@ namespace RTciv2.Forms
             if (FullscrBox.Checked) ResolBox.Enabled = false;
             else ResolBox.Enabled = true; }
 
+        //Path textbox text changed
+        private void PathBox_TextChanged(Object sender, EventArgs e) {
+            Civ2Path = PathBox.Text;
+            CheckPaths(); }
+
+        //SAV textbox text changed
+        private void SAVbox_TextChanged(Object sender, EventArgs e) {
+            SAVpath = Civ2Path + SAVbox.Text + ".SAV";
+            CheckPaths(); }
+
         //Start button clicked
         private void StartButton_Clicked(Object sender, EventArgs e)
         {
             //Check if directory & files exist
             ResultBox.Text = "";
-            if (!Directory.Exists(PathBox.Text)) ResultBox.AppendText("Directory doesn't exist.");
-            else
+            if (CheckPaths())
             {
-                string[] name = { PathBox.Text, SAVbox.Text, ".SAV" };
-                if (!File.Exists(String.Join("", name))) ResultBox.AppendText("SAV file doesn't exist.");
-                else //run the game!
-                {
-                    this.Hide();
-                    int resolChoice;
-                    if (FullscrBox.Checked) resolChoice = 0;
-                    else resolChoice = ResolBox.SelectedIndex + 1;
-                    var form2 = new MainCiv2Window(Resolutions[resolChoice], PathBox.Text, SAVbox.Text);
-                    form2.Closed += (s, args) => this.Close();
-                    form2.Show();
-                }
+                this.Hide();
+                int resolChoice;
+                if (FullscrBox.Checked) resolChoice = 0;
+                else resolChoice = ResolBox.SelectedIndex + 1;
+                var form2 = new MainCiv2Window(Resolutions[resolChoice], PathBox.Text, SAVbox.Text);
+                form2.Closed += (s, args) => this.Close();
+                form2.Show();
             }
         }
 
         //Quit button clicked
-        private void QuitButton_Clicked(Object sender, EventArgs e) {
-            Close(); }
+        private void QuitButton_Clicked(Object sender, EventArgs e) { Close(); }
+
+        //Check if directory and SAV file exist
+        private bool CheckPaths()
+        {
+            bool resultDir = Directory.Exists(Civ2Path);
+            bool resultSav = File.Exists(SAVpath);
+            ResultBox.Text = "";
+            if (!resultDir) 
+            {
+                ResultBox.AppendText("Directory doesn't exist.");
+                PathBox.BackColor = Color.PaleVioletRed;
+                SAVbox.BackColor = Color.PaleVioletRed;
+                SAVbox.Enabled = false;
+                StartButton.Enabled = false; 
+            }
+            else 
+            {
+                if (resultSav) 
+                {
+                    StartButton.Enabled = true;
+                    SAVbox.Enabled = true;
+                    PathBox.BackColor = Color.LightGray;
+                    SAVbox.BackColor = Color.LightGray;
+                }
+                else 
+                {
+                    ResultBox.AppendText("SAV file doesn't exist.");
+                    SAVbox.BackColor = Color.PaleVioletRed;
+                    StartButton.Enabled = false; 
+                } 
+            }
+            return resultDir && resultSav;
+        }
+
     }
 }
