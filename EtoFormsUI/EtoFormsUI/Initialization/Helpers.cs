6d27b7108bdb6ec817e1ec1a3212269974085b75
Reuben Evans <reubene@gmail.com>
2021-05-21 21:20:24 +1200

    Added support for starting new games based on mods in subfolders

--- EtoFormsUI/EtoFormsUI/Initialization/Helpers.cs
+++ EtoFormsUI/EtoFormsUI/Initialization/Helpers.cs
@@ -12,27 +12,45 @@ namespace EtoFormsUI.Initialization
                     var foundRules = new List<Ruleset>();
                     foreach (var searchPath in searchPaths)
                     {
-                        var rules = searchPath + Path.DirectorySeparatorChar + "rules.txt";
-                        if (File.Exists(rules))
+                        ValidateFolder(searchPath, searchPath, foundRules);
+                        foreach (var directory in Directory.EnumerateDirectories(searchPath))
                         {
-                            var game = searchPath + Path.DirectorySeparatorChar + "game.txt";
-                            var name = "Default";
-                            if (File.Exists(game))
-                            {
-                                foreach (var line in File.ReadLines(game))
-        
-                                {
-                                    if (!line.StartsWith("@title")) continue;
-                                    name = line[7..];
-                                    break;
-                                }
-                            }
-        
-                            foundRules.Add( new Ruleset { Name= name, FolderPath = searchPath, Root = searchPath });
+                            ValidateFolder(directory, searchPath, foundRules);
                         }
                     }
         
                     return foundRules;
                 }
+
+        private static void ValidateFolder(string searchPath, string rootPath, List<Ruleset> foundRules)
+        {
+            var rules = searchPath + Path.DirectorySeparatorChar + "rules.txt";
+            if (File.Exists(rules))
+            {
+                var game = searchPath + Path.DirectorySeparatorChar + "game.txt";
+                var name = "";
+                if (File.Exists(game))
+                {
+                    foreach (var line in File.ReadLines(game))
+
+                    {
+                        if (!line.StartsWith("@title")) continue;
+                        name = line[7..];
+                        break;
+                    }
+                }
+
+                if (string.IsNullOrWhiteSpace(name))
+                {
+                    name = Path.GetFileName(searchPath);
+                }
+                else if (searchPath != rootPath)
+                {
+                    name += " - " + Path.GetFileName(searchPath);
+                }
+
+                foundRules.Add(new Ruleset {Name = name, FolderPath = searchPath, Root = rootPath});
+            }
+        }
     }
 }
