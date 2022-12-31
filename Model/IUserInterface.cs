11e5bdb1a70a04ef351d9d3bfe88bd28f22471fd
Reuben Evans <revans@csod.com>
2022-12-31 19:42:19 +1300

    Erg

---
+++ Model/IUserInterface.cs
@@ -0,0 +1,12 @@
+﻿namespace Model;
+
+public interface IUserInterface
+{
+    bool CanDisplay(string? title);
+    InterfaceStyle Look { get; }
+    string Title { get; }
+    void Initialize();
+    IInterfaceAction ProcessDialog(string dialogName, DialogResult dialogResult);
+    IInterfaceAction ProcessFile(IEnumerable<string> filenames, bool ok);
+    IInterfaceAction GetInitialAction();
+}
