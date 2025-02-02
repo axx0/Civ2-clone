51345340ea8a361ebdd8fb10c8cebf433dd123be
sirloreal <gary.page3010@gmail.com>
2025-02-02 22:16:10 +0000

    Add initial tests

---
+++ Tests/Engine.Tests/LoadGameTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Engine.Tests;
+
+public class LoadGameTests
+{
+        // [Fact]
+        // public void TestLoadFrom()
+        // {
+        //     // Arrange
+        //     var path = "path/to/test/savefile.sav";
+        //     var mainApp = new MockMainApp();
+
+        //     // Act
+        //     //var result = LoadGame.LoadFrom(path, mainApp);
+
+        //     // Assert
+        //     Assert.NotNull(result);
+        //     // Add more assertions as needed
+        // }
+        
+        [Fact]
+        public void Test()
+        {
+            Assert.Fail("Test not yet implemented");
+        }
+    }
