axx0/Civ2-clone

commit ce355f3bc3eae6738d1c2cbcfc1aa4cd50d87b48

Keep track of city count using History instead of per-Civ counter
As discussed on https://github.com/axx0/Civ2-clone/issues/147

This also 'backfills' discovered advances in the History structure too.

I did manual tests similar to previous commits,
with both a MGE legacy SAV and a new-style json SAV file,
to verify that the new city dialog suggests the correct next-city
name from CITIES.TXT.

Engine/src/History/History.cs
@@ -10,7 +10,6 @@ namespace Civ2engine
         private readonly Game _game;
 
         private List<HistoryEvent> _events = new();
-        
 
         internal History(Game game)
         {

Engine/src/UnitActions/CityActions.cs
@@ -14,13 +14,13 @@ namespace Civ2engine.UnitActions
     {
         public static string GetCityName(Civilization civ , IGame game)
         {
-            var cityCount = civ.CitiesBuiltSoFar;
+            var cityCount = game.History.TotalCitiesBuilt(civ.Id);
             var names = game.CityNames;
             var tribe = civ.TribeName.ToUpperInvariant();
             var civCityList = names[names.ContainsKey(tribe) ? tribe : "EXTRA"];
             if (cityCount < civCityList?.Count)
             {
-                return civCityList[cityCount];
+                return civCityList[cityCount + 1];
             }
             
             return "Dummy Name";
@@ -44,7 +44,6 @@ namespace Civ2engine.UnitActions
             tile.CityHere = city;
             game.AllCities.Add(tile.CityHere);
             unit.Owner.Cities.Add(tile.CityHere);
-            unit.Owner.CitiesBuiltSoFar++;
 
             game.SetImprovementsForCity(city);
             

Engine/src/Game.cs
@@ -24,7 +24,7 @@ namespace Civ2engine
 
         public IHistory History
         {
-            get { return _history ??= new History(this); }
+            get { return _history ??= HistoryUtils.ReconstructHistory(this); }
         }
 
         public List<Civilization> AllCivilizations { get; } = new();
@@ -127,9 +127,7 @@ namespace Civ2engine
             var order = Rules.Orders.FirstOrDefault(t => t.Type == unitOrder);
             return order != null ? order.Name : Labels.For(LabelIndex.NoOrders);
         }
-
         
-
         public void ConnectPlayer(IPlayer player)
         {
             var id = player.Civilization.Id;

Engine/src/History/HistoryEventType.cs
@@ -5,4 +5,6 @@ namespace Civ2engine.Advances
         AdvanceDiscovered,
         CityBuilt
     }
+    // Breadcrumb: if you add a new HistoryEvent here, consider also updating
+    // HistoryUtils.ReconstructHistory to support 'backfilling' from legacy SAV/SCN files.
 }

Engine/src/History/HistoryUtils.cs
@@ -1,0 +1,37 @@
+namespace Civ2engine;
+
+public static class HistoryUtils
+{
+    /*
+     * Reconstructs an artificial History based on an initial game state.
+     *
+     * When creating a new Game, this ensures that initial technologies are recorded as discovered on turn 1.
+     *
+     * When creating a Game from a Legacy SAV/SCN file:
+     * The legacy SAV/SCN format does not persist turn-by-turn historical information,
+     * so this method allows events like CityBuilt and AdvanceDiscovered to be recorded in History.
+     * But, as a compromise all events will appear to have happened on the turn that the game was loaded.
+     */
+    public static History ReconstructHistory(Game game)
+    {
+        History history = new History(game);
+        foreach (var city in game.AllCities)
+        {
+            history.CityBuilt(city);
+        }
+        foreach (var civ in game.AllCivilizations)
+        {
+            if (civ.Advances != null)
+            {
+                for (int advanceNumber = 0; advanceNumber < civ.Advances.Length; advanceNumber++)
+                {
+                    if (civ.Advances[advanceNumber])
+                    {
+                        history.AdvanceDiscovered(advanceNumber, civ.Id);
+                    }
+                }
+            }
+        }
+        return history;
+    }
+}

Engine/src/OriginalSaves/Read.ClassicSav.cs
@@ -1019,7 +1019,6 @@ public class Read
             if (civ.PlayerType != PlayerType.Barbarians)
             {
                 int ofsetTcThisCiv = ofsetTc + 3 * civ.TribeId + 1;
-                civ.CitiesBuiltSoFar = bytes[ofsetTcThisCiv];
             }
         }
         #endregion

Model/Core/Civilization.cs
@@ -40,11 +40,6 @@ namespace Civ2engine
         public int[] CasualtiesPerUnitType { get; set; }
 
         public List<City> Cities { get; } = new();
-        /**
-         * CitiesBuiltSoFar is not necessarily the same as Cities.Count;
-         * it will include cities lost to other civs or destroyed.
-         */
-        public int CitiesBuiltSoFar { get; set; }
 
         public PlayerType PlayerType { get; set; }
         public int PowerRating { get; set; }

commit 0334da6151d9a3d4d99e2f5b1460e5232e711aa5

Fixes off-by-one city-naming bug introduced in commit ce355f3b
I thought I needed to increment by 1; it turns out there was a bug
in the way that I implemented classic SAV loading (see subsequent
commit).

This commit also adds a unit test (and introduces Moq for mocking).

Engine/src/History/History.cs
@@ -7,11 +7,11 @@ namespace Civ2engine
 {
     public class History : IHistory
     {
-        private readonly Game _game;
+        private readonly IGame _game;
 
         private List<HistoryEvent> _events = new();
 
-        internal History(Game game)
+        internal History(IGame game)
         {
             _game = game;
         }
@@ -34,7 +34,7 @@ namespace Civ2engine
 
     public class CityBuiltEvent : HistoryEvent
     {
-        public CityBuiltEvent(City city, Game game) : base(HistoryEventType.CityBuilt, city.Owner.Id, game)
+        public CityBuiltEvent(City city, IGame game) : base(HistoryEventType.CityBuilt, city.Owner.Id, game)
         {
             X = city.X;
             Y = city.Y;
@@ -52,7 +52,7 @@ namespace Civ2engine
     {
         public int Advance { get; }
 
-        public ResearchEvent(int advance, int civ, Game game) : base(HistoryEventType.AdvanceDiscovered, civ, game)
+        public ResearchEvent(int advance, int civ, IGame game) : base(HistoryEventType.AdvanceDiscovered, civ, game)
         {
             Advance = advance;
         }
@@ -64,7 +64,7 @@ namespace Civ2engine
         public int Civ { get; }
 
         public int Turn { get; }
-        protected HistoryEvent(HistoryEventType eventType, int civ, Game game)
+        protected HistoryEvent(HistoryEventType eventType, int civ, IGame game)
         {
             EventType = eventType;
             Civ = civ;

Engine/src/UnitActions/CityActions.cs
@@ -20,7 +20,7 @@ namespace Civ2engine.UnitActions
             var civCityList = names[names.ContainsKey(tribe) ? tribe : "EXTRA"];
             if (cityCount < civCityList?.Count)
             {
-                return civCityList[cityCount + 1];
+                return civCityList[cityCount];
             }
             
             return "Dummy Name";

Tests/Engine.Tests/Engine.Tests.csproj
@@ -32,6 +32,7 @@
   <ItemGroup>
     <PackageReference Include="coverlet.collector" Version="6.0.2" />
     <PackageReference Include="Microsoft.NET.Test.Sdk" Version="17.12.0" />
+    <PackageReference Include="Moq" Version="4.20.72" />
     <PackageReference Include="xunit" Version="2.9.2" />
     <PackageReference Include="xunit.runner.visualstudio" Version="2.8.2" />
   </ItemGroup>

Tests/Engine.Tests/UnitActions/CityActionsTest.cs
@@ -1,0 +1,64 @@
+using Civ2engine;
+using Civ2engine.UnitActions;
+using Model.Core;
+using Moq;
+
+namespace Engine.Tests.UnitActions;
+
+public class CityActionsTest
+{
+    private static Mock<IGame> SetupGame(int citiesToCreate, out Civilization civ)
+    {
+        // Given a game
+        var game = new Mock<IGame>();
+        // with a civilization named "TestCiv"
+        civ = new Civilization();
+        civ.TribeName = "TestCiv";
+        game.Setup(g => g.AllCivilizations).Returns([civ]);
+        // with a list of city names
+        Dictionary<string, List<string>> cityNames = new Dictionary<string, List<string>>();
+        cityNames["TESTCIV"] = ["City1", "City2", "City3"];
+        game.Setup(g => g.CityNames).Returns(cityNames);
+        // with N cities in the History
+        History history = new History(game.Object);
+        for (int cityCounter = 0; cityCounter < citiesToCreate; cityCounter++)
+        {
+            City c = new City();
+            c.Name = cityNames["TESTCIV"][cityCounter];
+            c.Owner = civ;
+            history.CityBuilt(c);
+        }
+        game.Setup(g => g.History).Returns(history);
+        return game;
+    }
+
+    [Fact]
+    public void GetCityNameForFirstCity()
+    {
+        // Given a game with zero cities already created
+        var game = SetupGame(0, out var civ);
+        // Then GetCityName should suggest the first city name
+        string suggested = CityActions.GetCityName(civ, game.Object);
+        Assert.Equal("City1", suggested);
+    }
+
+    [Fact]
+    public void GetCityNameForSecondCity()
+    {
+        // Given a game with one city already created
+        var game = SetupGame(1, out var civ);
+        // then GetCityName should suggest the second city name
+        string suggested = CityActions.GetCityName(civ, game.Object);
+        Assert.Equal("City2", suggested);
+    }
+
+    [Fact]
+    public void GetCityNameWhenCitiesListExhausted()
+    {
+        // Given a game with three cities already created
+        var game = SetupGame(3, out var civ);
+        // then GetCityName should suggest Dummy Name
+        string suggested = CityActions.GetCityName(civ, game.Object);
+        Assert.Equal("Dummy Name", suggested);
+    }
+}