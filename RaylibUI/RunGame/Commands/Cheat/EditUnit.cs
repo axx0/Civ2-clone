17d5fb8f9bf39c45b3346d18f3fa7850dfc46a31
Logan M Hood <hoodlm@protonmail.com>
2026-05-31 14:37:06 -0400

    Wire up placeholder dialogs for cheat menus

---
+++ RaylibUI/RunGame/Commands/Cheat/EditUnit.cs
@@ -0,0 +1,29 @@
+using Model.Input;
+using Civ2engine.IO;
+using Model.Core;
+using Model.Controls;
+
+namespace RaylibUI.RunGame.Commands.Cheat;
+
+public class EditUnit(GameScreen gameScreen) : AlwaysOnCommand(gameScreen, CommandIds.CheatEditUnit, [new Shortcut(Key.U, ctrl: true, shift: true)])
+{
+    private CivDialog? _placeholderDialog;
+
+    public override void Action()
+    {
+        _placeholderDialog = new CivDialog(GameScreen.Main, new DialogElements(new PopupBox
+        {
+            Title = GetType().Name,
+            Text = [$"This is a placeholder for {Command?.GameCommand?.Id}, which is not yet implemented."],
+            LineStyles = [TextStyles.Left],
+            Button = [Labels.Ok]
+        }), PlaceholderHandler);
+
+        GameScreen.ShowDialog(_placeholderDialog);
+    }
+
+    private void PlaceholderHandler(string button, int selection, IList<bool>? arg3, IDictionary<string, string>? arg4)
+    {
+        GameScreen.CloseDialog(_placeholderDialog);
+    }
+}
