386ea222dc04f54c3a5d54a9b2ba5a6e8c21d02f
Logan M Hood <hoodlm@protonmail.com>
2026-06-14 13:57:08 -0400

    Cheat: SetGameYear
    Also requires making Game.TurnNumber externally mutable

--- RaylibUI/RunGame/Commands/Cheat/SetGameYear.cs
+++ RaylibUI/RunGame/Commands/Cheat/SetGameYear.cs
@@ -2,28 +2,63 @@ using Model.Input;
 using Civ2engine.IO;
 using Model.Core;
 using Model.Controls;
+using Raylib_CSharp.Logging;
 
 namespace RaylibUI.RunGame.Commands.Cheat;
 
 public class SetGameYear(GameScreen gameScreen) : AlwaysOnCommand(gameScreen, CommandIds.CheatSetGameYear, [new Shortcut(Key.F4, shift: true)])
 {
-    private CivDialog? _placeholderDialog;
+    private CivDialog? _setGameYearDialog;
+    private const string NewTurnNumber = "New Turn Number";
 
     public override void Action()
     {
-        _placeholderDialog = new CivDialog(GameScreen.Main, new DialogElements(new PopupBox
-        {
-            Title = GetType().Name,
-            Text = [$"This is a placeholder for {Command?.GameCommand?.Id}, which is not yet implemented."],
-            LineStyles = [TextStyles.Left],
-            Button = [Labels.Ok]
-        }), PlaceholderHandler);
-
-        GameScreen.ShowDialog(_placeholderDialog);
+        _setGameYearDialog = BuildSetGameYearDialog(GameScreen.Game.TurnNumber);
+        GameScreen.ShowDialog(_setGameYearDialog);
     }
 
-    private void PlaceholderHandler(string button, int selection, IList<bool>? arg3, IDictionary<string, string>? arg4)
+    private CivDialog BuildSetGameYearDialog(int currentTurn)
     {
-        GameScreen.CloseDialog(_placeholderDialog);
+        String dialogTitle = $"Change # Of Turns Elapsed";
+        String subtext = $"# Turns Elapsed: {currentTurn}.";
+
+        TextBoxDefinition textBox = new TextBoxDefinition
+        {
+            Index = 0,
+            InitialValue = currentTurn.ToString(),
+            Description = "New # turns:",
+            MinValue = int.MinValue,
+            CharLimit = 11,
+            Name = NewTurnNumber,
+            Width = 225
+        };
+
+        PopupBox popupBox = new PopupBox
+        {
+            Title = dialogTitle,
+            Button = [Labels.Ok, Labels.Cancel],
+            Text = new List<string> { subtext },
+            LineStyles = new List<TextStyles> { TextStyles.LeftOwnLine }
+        };
+
+        return new CivDialog(
+            GameScreen.Main, new DialogElements(popupBox) { TextBoxes = new List<TextBoxDefinition> { textBox } }, 
+            DoChangeYear);
+    }
+
+    private void DoChangeYear(string button, int selection, IList<bool>? arg3, IDictionary<string, string>? textBoxes)
+    {
+        {
+            if (textBoxes != null && button == Labels.Ok &&
+                textBoxes.TryGetValue(NewTurnNumber, out string? newTurnNumber))
+            {
+                if (int.TryParse(newTurnNumber, out int parsedTurnNumber))
+                {
+                    GameScreen.Game.TurnNumber = parsedTurnNumber;
+                }
+            }
+            GameScreen.CloseDialog(_setGameYearDialog);
+            GameScreen.StatusPanel.Update();
+        }
     }
 }
