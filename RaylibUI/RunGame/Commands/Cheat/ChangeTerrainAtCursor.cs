78827aaa8c5a436b270d8874aa5bd7e499b3357a
Logan M Hood <hoodlm@protonmail.com>
2026-06-14 13:58:35 -0400

    Cheat: ChangeTerrainAtCursor partially implemented - can change terrain kind, but not terrain features/improvements

--- RaylibUI/RunGame/Commands/Cheat/ChangeTerrainAtCursor.cs
+++ RaylibUI/RunGame/Commands/Cheat/ChangeTerrainAtCursor.cs
@@ -1,29 +1,48 @@
+using Civ2engine;
 using Model.Input;
 using Civ2engine.IO;
 using Model.Core;
 using Model.Controls;
+using Model.Core.Mapping;
 
 namespace RaylibUI.RunGame.Commands.Cheat;
 
+// TODO: For parity with civ2, this needs a second dialog for editing terrain features (improvements, river, etc.) not just terrain type.
 public class ChangeTerrainAtCursor(GameScreen gameScreen) : AlwaysOnCommand(gameScreen, CommandIds.CheatChangeTerrainAtCursor, [new Shortcut(Key.F8, shift: true)])
 {
-    private CivDialog? _placeholderDialog;
+    private Tile? cursorTile;
+    private CivDialog? _selectNewTerrainKind;
+    private Terrain[]? terrains;
 
     public override void Action()
     {
-        _placeholderDialog = new CivDialog(GameScreen.Main, new DialogElements(new PopupBox
+        cursorTile = ((Game)GameScreen.Game).ActiveTile;
+        var mapNumber = cursorTile.Map.MapIndex;
+        terrains = GameScreen.Game.Rules.Terrains[mapNumber];
+        var currentTerrain = cursorTile.Terrain;
+        
+        var terrainSelection = new PopupBox
         {
-            Title = GetType().Name,
-            Text = [$"This is a placeholder for {Command?.GameCommand?.Id}, which is not yet implemented."],
-            LineStyles = [TextStyles.Left],
-            Button = [Labels.Ok]
-        }), PlaceholderHandler);
+            Title = "SelectTerrainType",
+            Options = terrains.Select(t => t.Type.ToString()).ToArray(),
+            Default = Array.IndexOf(terrains, currentTerrain),
+            Button = [Labels.Ok, Labels.Cancel]
+        };
+        
+        _selectNewTerrainKind = new CivDialog(GameScreen.Main, new DialogElements(terrainSelection), ChangeTerrainHandler);
 
-        GameScreen.ShowDialog(_placeholderDialog);
+        GameScreen.ShowDialog(_selectNewTerrainKind);
     }
 
-    private void PlaceholderHandler(string button, int selection, IList<bool>? arg3, IDictionary<string, string>? arg4)
+    private void ChangeTerrainHandler(string button, int selection, IList<bool>? arg3, IDictionary<string, string>? arg4)
     {
-        GameScreen.CloseDialog(_placeholderDialog);
+        if (button == Labels.Ok && null != cursorTile && null != terrains)
+        {
+            cursorTile.Terrain = terrains[selection];
+            GameScreen.StatusPanel.Update();
+            GameScreen.TileCache.Clear();
+            GameScreen.MapControl.ForceRedraw = true;
+        }
+        GameScreen.CloseDialog(_selectNewTerrainKind);
     }
 }
